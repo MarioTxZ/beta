b45a1fa0224699324f9c59a9552e1e895bbf83a7
Mario <marodriguez.7600@unicesmag.edu.co>
2024-10-10 12:00:14 -0500

    beta

--- api/src/backend/signup.php
+++ api/src/backend/signup.php
@@ -6,11 +6,23 @@
     $email = $_POST['email'];
     $pass = $_POST['passwd'];
     $enc_pass = md5($pass);
+    //validate if email already exists
+    $query = "SELECT * FROM users WHERE email = '$email'";
+    $result = pg_query($conn, $query);
+    $row = pg_fetch_assoc($result);
+    if($row) {
+        echo "<script>alert('Email already exists')</script>";
+        header('refresh:0; url=http://127.0.0.1/beta/api/src/register_form.html');
+        exit();
+    
+    }
+     
     //query to insert data into users table
     $query = "INSERT INTO users (email, password) VALUES ('$email', '$enc_pass')";
     $result = pg_query($conn, $query);
     if($result) {
-        echo 'registration successful';
+        echo "<script>alert('registration successfully')</script>";
+        header('refresh:0; url=http://127.0.0.1/beta/api/src/login_form.html');
     } else {
         echo 'Registration failed';
     }
